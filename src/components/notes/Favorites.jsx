f531b778360a0a96977269832e18c3b854b3ead5
muddassirk <mkkhanu72@gmail.com>
2021-06-30 14:52:24 +0500

    create favorite page

---
+++ src/components/notes/Favorites.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect } from 'react-redux-firebase'
+import NoteList from './NoteList'
+
+const Favorites = () => {
+
+    useFirestoreConnect([{collection:'notes',where:['favorite','==',true], orderBy:['createdAt','desc'],storeAs:'favnotes'}])
+    const favnotes = useSelector(state => state.firestore.ordered.favnotes)
+    // console.log('favnotes', favnotes);
+
+    return (<NoteList notes={favnotes}/>
+    )
+}
+
+export default Favorites
